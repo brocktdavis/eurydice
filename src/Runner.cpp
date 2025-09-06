127961b0b63de16d6b9e66c5e6714b62a7ff49f0
Brock Davis <btdavis9999@gmail.com>
2025-09-06 17:33:05 -0500

    Playback work in progres

--- src/Runner.cpp
+++ src/Runner.cpp
@@ -1,6 +1,7 @@
 #include "Runner.hpp"
 #include <tuple>
 #include <algorithm>
+#include <iostream>
 
 Runner::Runner() {
     _audio = new RtAudio();
@@ -65,8 +66,57 @@ Runner::DeviceDefinition Runner::getDefaultDevice(void) {
     }
 }
 
+struct CallbackData {
+    AbstractSample* sample;
+    unsigned int sampleRate;
+};
+
+// Static callback function that can be converted to RtAudioCallback
+int audioCallback(void *outputBuffer, void*, unsigned int nBufferFrames, double streamTime, RtAudioStreamStatus, void *userData) {
+    CallbackData* data = static_cast<CallbackData*>(userData);
+    AbstractSample* sample = data->sample;
+    unsigned int sampleRate = data->sampleRate;
+    
+    float *buffer = static_cast<float *>(outputBuffer);
+    std::cout << "[audioCallback] streamTime: " << streamTime << ", nBufferFrames: " << nBufferFrames << ", sampleRate: " << sampleRate << std::endl;
+    for (unsigned int i = 0; i < nBufferFrames; ++i) {
+        const double additionalTime = static_cast<double>(i) / sampleRate;
+        std::cout << "i: " << i << ", additionalTime: " << additionalTime << std::endl;
+        buffer[i] = sample->getFrame(streamTime + additionalTime).RTAUDIO_FLOAT32;
+    }
+    return 0;
+}
+
 void Runner::playSample(AbstractSample &sample) {
-    std::cout << "Play sample" << sample << std::endl;
+    std::cout << "Play sample " << sample << std::endl;
+
+    Runner::DeviceDefinition defaultDevice = getDefaultDevice();
+
+    RtAudio::StreamParameters params;
+    params.deviceId = defaultDevice.rtAudioId;
+    params.nChannels = 1;
+
+    const unsigned int sampleRate = defaultDevice.currentSampleRate;
+    unsigned int bufferFrames = 256;
+
+    CallbackData callbackData = {&sample, sampleRate};
+
+    RtAudioErrorType result = _audio->openStream(
+        &params,
+        nullptr, // input params
+        RTAUDIO_FLOAT32,
+        sampleRate,
+        &bufferFrames,
+        audioCallback,
+        &callbackData
+    );
+    
+    if (result != RTAUDIO_NO_ERROR) {
+        std::cerr << "Failed to open audio stream: " << _audio->getErrorText() << std::endl;
+        return;
+    }
+    
+    std::cout << "Audio stream opened successfully" << std::endl;
 }
 
 
